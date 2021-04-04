75d779b454641327e45471d81f15e6c4c2107899
ibnuhafizzh <hafizhputraa@gmail.com>
2021-04-04 19:51:04 +0700

    add main

--- InputFile.py
+++ InputFile.py
@@ -9,7 +9,7 @@ arrFile = []
 with open(filename,'r') as file:
     for line in file:
         arrFile.append(line.replace('\n', '').replace('(','').replace(')',''))
-        print(line,end="")
+        # print(line,end="")
      
             #if line.startswith('-'):
             #    continue
@@ -19,7 +19,8 @@ X = 0
 Y = 0
 Namasimpul = str("")
 ArrSimpul = []
-for i in range (1, int(arrFile[0])+1):
+Nnode = int(arrFile[0])
+for i in range (1, Nnode+1):
     #print(arrFile[i])
     stringTempCoor = str("")
     for j in range (len(arrFile[i])):
@@ -34,9 +35,30 @@ for i in range (1, int(arrFile[0])+1):
         elif (arrFile[i][j] != ',' and arrFile[i][j] != ' '):
             stringTempCoor = stringTempCoor+arrFile[i][j]
             NamaSimpul = stringTempCoor
+    ArrSimpul.append([NamaSimpul,X,Y])
 
-    ArrSimpul.append((NamaSimpul,X,Y))
+connected = []
+for i in range(Nnode+1, len(arrFile)):
+    tempangka = 0
+    tempbobot = []
+    stringTempCoor = str("")
+    for j in range(len(arrFile[i])):
+        if (arrFile[i][j] != ' '):
+            tempangka = int(stringTempCoor)
+            stringTempCoor = ""
+        elif (arrFile[i][j] != ' ' and arrFile[i][j] != '0'):
+            stringTempCoor = stringTempCoor+arrFile[i][j]
+    connected.append(tempbobot)
+# for l in range (5):
+#     for k in range (3):
+#         print(arrFile[l][k])
+        # if (arrFile[i][k] != '0' or arrFile[i][k] != ' '):
+        #     connected.append([ArrSimpul[k][0], arrFile[i][k]])
+print()
+
+
 print(ArrSimpul)    
-print(ArrSimpul[0][0]) 
-print(ArrSimpul[0][1]) 
-print(ArrSimpul[0][2])  
+print(connected)
+# print(ArrSimpul[0][0]) 
+# print(ArrSimpul[0][1]) 
+# print(ArrSimpul[0][2])  

7ca82734d94c386b1d574b27ff78c8318b901770
ibnuhafizzh <hafizhputraa@gmail.com>
2021-04-05 19:26:34 +0700

    .

--- testmain.py
+++ testmain.py
@@ -4,7 +4,8 @@ from InputFile import ArrSimpul
 import math
 
 #Parsing file menjadi array of node, format : [Name, X, Y, {Neighbors : Bobot}]
-def inputFile(filename):
+def inputFile():
+    filename = str(input("Masukan Nama File (Nama.txt): ")) 
     arrFile = []
 
     #Loop masukin baris ke array arrFile
@@ -70,10 +71,6 @@ def inputFile(filename):
             if (temp[l] != 0):
                 neightbors[l] = temp[l]
         (ArrSimpul[i]).append(neightbors)
-
-    # print("list of node [Name, X, Y, {Neighbors : Bobot}]\n")
-    # for i in range (len(ArrSimpul)):
-    #     print("Node", i+1,  ":" ,ArrSimpul[i])
     return (ArrSimpul)
 
 class Node:
@@ -136,67 +133,10 @@ class Graph:
         nodes = s1.union(s2)
         return list(nodes)
 
-# A* search
-def astar_search(graph, heuristics, start, end):
-    
-    # Create lists for open nodes and closed nodes
-    open = []
-    closed = []
-    # Create a start node and an goal node
-    start_node = Node(start, None)
-    goal_node = Node(end, None)
-    # Add the start node
-    open.append(start_node)
-    
-    # Loop until the open list is empty
-    while len(open) > 0:
-        # Sort the open list to get the node with the lowest cost first
-        open.sort()
-        # Get the node with the lowest cost
-        current_node = open.pop(0)
-        # Add the current node to the closed list
-        closed.append(current_node)
-        
-        # Check if we have reached the goal, return the path
-        if current_node == goal_node:
-            path = []
-            while current_node != start_node:
-                path.append(current_node.name + ': ' + str(current_node.g))
-                current_node = current_node.parent
-            path.append(start_node.name + ': ' + str(start_node.g))
-            # Return reversed path
-            return path[::-1]
-        # Get neighbours
-        neighbors = graph.get(current_node.name)
-        # Loop neighbors
-        for key, value in neighbors.items():
-            # Create a neighbor node
-            neighbor = Node(key, current_node)
-            # Check if the neighbor is in the closed list
-            if(neighbor in closed):
-                continue
-            # Calculate full path cost
-            neighbor.g = current_node.g + graph.get(current_node.name, neighbor.name)
-            neighbor.h = heuristics.get(neighbor.name)
-            neighbor.f = neighbor.g + neighbor.h
-            # Check if neighbor is in open list and if it has a lower f value
-            if(add_to_open(open, neighbor) == True):
-                # Everything is green, add neighbor to open list
-                open.append(neighbor)
-    # Return None, no path is found
-    return None
-
-# Check if a neighbor should be added to open list
-def add_to_open(open, neighbor):
-    for node in open:
-        if (neighbor == node and neighbor.f > node.f):
-            return False
-    return True
 
 def main():
-    filename = str(input("Masukan Nama File (Nama.txt): "))         #Masukan nama file dengan format "Nama.txt"
-    file = inputFile(filename)
-    # print(file)
+    file = inputFile()
+    print(file)
     arrNode = []
     for i in range (len(file)):
         arrNode.append(Node(file[i][0], None, file[i][1], file[i][2], file[i][3]))
